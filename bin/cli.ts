e3d8cdc25c8c7ee78f3159eabb0bdeeae0227ec8
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2020-02-06 14:40:37 +0900

    feat:install cac

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
 import index from '../src/index'
+import cac from 'cac'
 
-index()
+const cli = cac()
+
+cli.command('hello [name]', 'Enter your name').action(() => {
+  index()
+})
+
+cli.help()
+cli.parse()
